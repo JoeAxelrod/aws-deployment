bb6391e086997658dd6be74735e70540e1a07539
joe <jossefk1@gmail.com>
2024-01-05 09:57:37 +0200

    Update response message in root route to 'Hello World! 222222'.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 11111');
+  res.send('Hello World! 222222');
 });
 
 app.get('/tables', async (req, res) => {
