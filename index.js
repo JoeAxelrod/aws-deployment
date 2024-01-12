155de43d943a460bf31785ecc3cb20c8eb65ba61
joe <jossefk1@gmail.com>
2024-01-12 22:08:50 +0200

    Update index.js to send 'Hello World! 333333' instead of 'Hello World! 222222'.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 222222');
+  res.send('Hello World! 333333');
 });
 
 app.get('/tables', async (req, res) => {
